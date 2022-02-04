7682175c272791eebefc3c61bc70e1ce0910365e
mazdak1987 <99016100+mazdak1987@users.noreply.github.com>
2022-02-04 13:53:31 +0100

    trying to add text overlay on render_panel_

--- src/myviz.cpp
+++ src/myviz.cpp
@@ -94,6 +94,48 @@ void MyViz::initializeRViz()
   manager_ = new rviz_common::VisualizationManager(render_panel_, rviz_ros_node_, this, clock);
   render_panel_->initialize(manager_);
   app_->processEvents();
+  
+  
+  //trying to add text overlay on render_panel_
+  QRect screenGeo = QRect(20, 20, 200,200);//QGuiApplication::primaryScreen()->geometry()
+  auto hudTexture = Ogre::TextureManager::getSingleton().createManual(
+        "HUDTexture",        // name
+        Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+        Ogre::TEX_TYPE_2D,   // type
+        screenGeo.width(), screenGeo.height(),
+        0,                   // number of mipmaps
+        Ogre::PF_A8R8G8B8,   // pixel format chosen to match a format Qt can use
+        Ogre::TU_DEFAULT    //TU_DEFAULT usage
+        );
+  Ogre::MaterialPtr HudMaterial = Ogre::MaterialManager::getSingleton().create("HUDMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+  HudMaterial->getTechnique(0)->getPass(0)->createTextureUnitState("HUDTexture");
+  HudMaterial->getTechnique(0)->getPass(0)->setSceneBlending(Ogre::SBT_MODULATE);
+  Ogre::OverlayManager& overlayManager = Ogre::OverlayManager::getSingleton();
+  auto overlay = overlayManager.create("OverlayName");
+  Ogre::PanelOverlayElement* overlayElement = static_cast<Ogre::PanelOverlayElement*>(overlayManager.createOverlayElement("Panel", "PanelName"));// Create a panel
+  overlayElement->setMetricsMode(Ogre::GMM_PIXELS);
+  overlayElement->setPosition(0, 0);
+  overlayElement->setDimensions(screenGeo.width(), screenGeo.height());
+  overlayElement->setMaterialName("HUDMaterial");
+  overlay->add2D(overlayElement);//Add the panel to the overlay
+
+  Ogre::HardwarePixelBufferSharedPtr pixelBuffer = hudTexture->getBuffer();// Get the pixel buffer
+  pixelBuffer->lock( Ogre::HardwareBuffer::HBL_DISCARD ); //Lock the pixel buffer (for best performance use HBL_DISCARD!) // HBL_NORMAL
+  const Ogre::PixelBox& pixelBox = pixelBuffer->getCurrentLock(); //get a pixel box
+  Ogre::uint8* pDest = static_cast<Ogre::uint8*>(pixelBox.data);
+  memset(pDest, 0, hudTexture->getWidth() * hudTexture->getHeight() * 4);// (uint8 * 4 = uint32) the buffer content is the colors R,G,B,A. Filling with zeros gets a 100% transparent image
+  QImage Hud(pDest, hudTexture->getWidth(), hudTexture->getHeight(), QImage::Format_ARGB32); // tell QImage to use OUR buffer and a compatible image buffer format
+  QPainter painter(&Hud);
+  QRect borderRect = QRect(10, 10, 200, 200);
+  QFont font;
+  font.setPointSize(40);
+  painter.setFont(font);
+  painter.setPen(Qt::red);
+  painter.drawText(borderRect, Qt::AlignLeft, "my_text_to_show");
+  //
+  
+  
+  
   manager_->initialize();
   manager_->startUpdate();
 }
